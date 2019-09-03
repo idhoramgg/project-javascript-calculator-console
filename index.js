1464bc9e681e649b40a066f61b77a1794602593d
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-03 19:31:29 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict"
+
+let number1 = 50
+let number2 = 20
+
+let addition = number1 + number2
+let substract = number1 - number2
+let multiplication = number1 * number2
+let division = number1 / number2
+
+console.log(number1)
+console.log(number2)
+
+console.log(addition)
+console.log(substract)
+console.log(multiplication)
+console.log(division)
+
